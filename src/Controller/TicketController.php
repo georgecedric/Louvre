341a84a521bfe717ef015ceed0285d5b66b2f201
georgecedric <george.cedric@hotmail.com>
2019-01-17 16:01:49 +0100

    creation du projet billetterie du louvre

---
+++ src/Controller/TicketController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TicketController extends AbstractController
+{
+    /**
+     * @Route("/ticket", name="ticket")
+     */
+    public function index()
+    {
+        return $this->render('ticket/index.html.twig', [
+            'controller_name' => 'TicketController',
+        ]);
+    }
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('ticket/home.html.twig' ); 
+    }
+
+    /**
+     * @Route("/ticket/firststage", name="firststage")
+     */
+    public function firststage()
+    {
+        return $this->render('ticket/firststage.html.twig' ); 
+    }
+}
